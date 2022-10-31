982749f485eceefdf49d07219f42a0e48dc13fe9
Natasha <njohnson533@gmail.com>
2022-10-31 12:48:40 -0400

    allow user to input decimal rates

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: "./typescript-mortgage-calc"
+  // assetPrefix: "./typescript-mortgage-calc"
+  assetPrefix: "./"
 }
 
 export default nextConfig;
